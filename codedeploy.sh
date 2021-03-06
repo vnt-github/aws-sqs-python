7ca33a15625bd3f580176286ab53b678bd863036
vnt@wsl <vineetbharot@gmail.com>
2021-03-05 20:23:45 -0800

    code deploy fixes

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,7 +1,7 @@
 sudo apt update
-sudo apt install ruby-full
+sudo apt install -y ruby-full
 sudo apt install wget
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto
+sudo ./install auto > /tmp/logfile
